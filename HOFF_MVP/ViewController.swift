3017372ce960c88c0a72b35ead6ed45d7191340e
Aselder <asover98@gmail.com>
2020-09-13 14:56:03 +0300

    Initial Commit

---
+++ HOFF_MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HOFF_MVP
+//
+//  Created by Асельдер on 13.09.2020.
+//  Copyright © 2020 Асельдер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
